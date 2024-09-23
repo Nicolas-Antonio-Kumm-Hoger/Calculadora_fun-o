f3e94965d4b11539485d23b8b8ff57c5b62975ac
Nicolas-Antonio-Kumm-Hoger <159266658+Nicolas-Antonio-Kumm-Hoger@users.noreply.github.com>
2024-09-23 09:37:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcular(){
+    // Pegando valor coeficiente do html
+    const num_a = parseFloat(document.getElementById('num_a').value)
+    const num_b = parseFloat(document.getElementById('num_b').value)
+    const num_c = parseFloat(document.getElementById('num_c').value)
+    let resultado;
+
+
+
+
+
+
+}
